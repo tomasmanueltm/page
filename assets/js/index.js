b5c34595dac37fe07c7d480f54d0dd0ac707fed9
TomasManueltm <antoniomanuelbaptistabaptista@gmail.com>
2022-06-17 16:55:42 +0100

    servico, index

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,10 @@
+let $ = (e) => document.querySelector(e),   
+    btnToggle = $(".btn-toogle"),
+    ulToggle = $("header ul")
+    ;
 
-console.log("Tomas Manuel");
+
+btnToggle.addEventListener("click", function(){
+    
+    ulToggle.classList.add("block", "keyUl");
+})
